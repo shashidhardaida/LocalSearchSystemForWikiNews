c7deb56eff22cec8ebc074491d73bb92331aee8c
shashi1995 <shashidharreddydaida@gmail.com>
2020-07-08 00:51:20 -0500

    Project structure improved
    Deleted unncessary apps and made only two apps for the project

--- DjangoProject/LocalSearchSystem/LocalSearchSystem/settings.py
+++ DjangoProject/LocalSearchSystem/LocalSearchSystem/settings.py
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'UserManagementApp',
-    'ItemManagementApp',
-    'WebScrappingApp',
-    'SearchApp',
+    'Users',
+    'WikiNews'
     
 ]
 
