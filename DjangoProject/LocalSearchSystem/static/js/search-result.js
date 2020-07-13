8adb18980c4fe829c5866100f26cf1c49629785a
shashi1995 <shashidharreddydaida@gmail.com>
2020-07-13 17:56:05 -0500

    Changes from teat mates
    Improvement work

--- DjangoProject/LocalSearchSystem/static/js/search-result.js
+++ DjangoProject/LocalSearchSystem/static/js/search-result.js
@@ -5,7 +5,7 @@ function searchResult() {
 }
 
 function fetchResults(searchQuery) {
-    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    const endpoint = `https://en.wikinews.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
   	fetch(endpoint)
   		.then(response => response.json())
   		.then(data => {
@@ -23,15 +23,15 @@ function displayResults(results) {
   searchResults.innerHTML = '';
   // Loop over results array
   results.forEach(result => {
-  const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
+  const url = encodeURI(`https://en.wikinews.org/wiki/${result.title}`);
   localStorage.setItem("URL", url); 
   searchResults.insertAdjacentHTML('beforeend',
       `<div class="resultItem">
         <h3 class="resultItem-title">
-          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+         <a href="search-item-result.html" target="_blank" rel="noopener">${result.title}</a>
         </h3>
         <span class="resultItem-snippet">${result.snippet}</span><br>
-        <a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+        <a href="search-item-result.html" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
       </div>`
     );
   });
