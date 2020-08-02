779f4f1041a1fc1f5fedea78f7b45197513bc2a5
shashi1995 <shashidharreddydaida@gmail.com>
2020-08-02 03:05:07 -0500

    search results with paragraphs

--- DjangoProject/LocalSearchSystem/WikiNews/search_indexes.py
+++ DjangoProject/LocalSearchSystem/WikiNews/search_indexes.py
@@ -6,6 +6,7 @@ class WikiNewsItemIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     id = indexes.IntegerField(model_attr='id')
+    content = indexes.CharField(model_attr='text')
 
     def get_model(self):
         return WikiNewsItem
